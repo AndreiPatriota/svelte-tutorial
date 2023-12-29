182c1a6a0330ccb2e393006bd8154985de22da4d
Andrei Patriota <63430047+AndreiPatriota@users.noreply.github.com>
2023-12-28 21:24:26 -0300

    Completed

---
+++ src/Inner.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    function sayHello() {
+        dispatch('greet', {
+            greet: 'Hello, Mom!'
+        });
+    }
+</script>
+
+<button on:click={sayHello}>
+    Click to say hello
+</button>
