484749b8d14bf8373d779818b9305f2a0c58a5da
Andrei Patriota <63430047+AndreiPatriota@users.noreply.github.com>
2023-12-29 22:25:28 -0300

    complete without boilerplate

--- src/App.svelte
+++ src/App.svelte
@@ -3,18 +3,9 @@
   import Decrementer from "./Decrementer.svelte";
   import Incrementer from "./Incrementer.svelte";
   import Resetter from "./Resetter.svelte";
-  import { onDestroy } from "svelte";
-
-  let count_value;
-
-  const unsubscribe = count.subscribe((value)=>{
-    count_value = value;
-  })
-
-  onDestroy(unsubscribe);
 </script>
 
-<h1>The count value is {count_value}</h1>
+<h1>The count value is {$count}</h1>
 <Incrementer/>
 <Decrementer/>
 <Resetter/>
