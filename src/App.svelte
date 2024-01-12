296b4e5ae070b6325ed1ec09b112d07aff0b266b
Andrei Patriota <63430047+AndreiPatriota@users.noreply.github.com>
2024-01-11 23:09:29 -0300

    completed

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,24 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import { tweened } from "svelte/motion";
+    import { cubicOut } from "svelte/easing";
+
+    const progress = tweened(0, {
+        duration:400,
+        easing: cubicOut
+    })
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<progress value={$progress}/>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<button on:click={()=> progress.set(0)}>0%</button>
+<button on:click={()=> progress.set(0.25)}>25%</button>
+<button on:click={()=> progress.set(0.50)}>50%</button>
+<button on:click={()=> progress.set(0.75)}>75%</button>
+<button on:click={()=> progress.set(1)}>100%</button>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	progress {
+		display: block;
+		width: 100%;
+	}
 </style>
