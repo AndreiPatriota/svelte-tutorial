f649035355e4ee1bc0eff717678a220649ddb543
Andrei Patriota <63430047+AndreiPatriota@users.noreply.github.com>
2024-01-22 19:27:48 -0300

    completed

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,55 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { elasticOut } from 'svelte/easing';
+	import { fade } from 'svelte/transition';
+
+	let visible = false;
+
+    // Defines a custom animation
+	function spin(node, { duration }) {
+		return {
+            delay: 1000,
+			duration,
+			css: (t) => {
+                const eased = elasticOut(t);
+
+                return `
+                    transform: scale(${eased}) rotate(${eased*1080}deg);
+                    color: hsl(
+                        ${Math.trunc(t*360)},
+                        ${Math.min(100, 1000*(1-t))}%,
+                        ${Math.min(50, 500*(1-t))}%
+                    );`
+            }
+		};
+	}
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<label>
+	<input type="checkbox" bind:checked={visible} />
+	visible
+</label>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+{#if visible}
+	<div
+		class="centered"
+		in:spin={{ duration: 8000 }}
+		out:fade
+	>
+		<span>transitions!</span>
+	</div>
+{/if}
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	.centered {
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	span {
+		position: absolute;
+		transform: translate(-50%, -50%);
+		font-size: 4em;
+	}
 </style>
