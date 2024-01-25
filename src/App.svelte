0995df43bbc793cf4c220e7db2b393e79047c3ae
Andrei Patriota <63430047+AndreiPatriota@users.noreply.github.com>
2024-01-24 21:47:37 -0300

    Revert "Merge pull request #1 from AndreiPatriota/transition-events"
    This reverts commit 466623ced5ed9bfbc54b77b3a125d02039008163, reversing
    changes made to 1b6ca158ab1f2b5b42f12ee363a5a5aaac7f5797.

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,47 @@
 <script>
-	let visible = false;
-
-	function typewriter(node, { speed = 1 }) {
-		const valid = node.childNodes.length === 1 && node.childNodes[0].nodeType === Node.TEXT_NODE;
-
-		if (!valid) {
-			throw new Error(`This transition only works on elements with a single text node child`);
-		}
-
-        const text = node.textContent;
-        const duration = text.length / (speed * 0.01);
-
-		return {
-            duration,
-            tick: (t) => {
-                const i = Math.trunc(text.length * t);
-                node.textContent = text.slice(0, i);
-            }
-        };
-	}
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
 </script>
 
-<label>
-	<input type="checkbox" bind:checked={visible} />
-	visible
-</label>
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
 
-{#if visible}
-	<p transition:typewriter>
-		The quick brown fox jumps over the lazy dog
-	</p>
-{/if}
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
